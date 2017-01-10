44e5bc73244fc44db40e6ba4eab3ea364ede2cd2
shamaul-d <sdilmohamed@stuy.edu>
2017-01-10 00:25:57 -0500

    login/register done! (sorta...)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, request, render_template, session, url_for
 import os
-#from utils import database
+from utils import database
 import hashlib
 
 
@@ -31,30 +31,30 @@ def auth():
     if 'register' in request.form:
         if (request.form['user'] == '' or request.form['pass'] == ''):
             return render_template('login.html', msg = 'please fill in all forms of info')
-        elif (db_builder.check(request.form['user'])):
+        elif (database.logincheck(request.form['user'])):
             return render_template('login.html', msg = 'username taken, please choose a new one')
         else:
-            user0 = request.form["username"]
-            pass0 = hashp(request.form["password"])
-            db_builder.add_user(user0,pass0)
-            flash("new account created")
-            return redirect(url_for("login"))
+            user0 = request.form['user']
+            pass0 = hashp(request.form['pass'])
+            database.adduser(user0,pass0, "placeholder", "placeholder", 0)
+            ## what are we doing here?
+            return render_template('login.html', msg = 'new account created')
     ## login
     else:
-        if (not (db_builder.check(request.form['username']))):
-            flash("username does not exist")
-            return redirect(url_for("login"))
-        elif (db_builder.get_hash(request.form['username']) == hashp(request.form['password'])):
-            user1 = request.form['username']
-            session['username'] = user1
-            print '!SESSION_STATUS: ' + session['username']
-            return redirect(url_for("home"))
+        if (not (database.logincheck(request.form['user']))):
+            return render_template('login.html', msg = 'username does not exist')
+        elif (database.gethash(request.form['user']) == hashp(request.form['pass'])):
+            user1 = request.form['user']
+            session['user'] = user1
+            return redirect(url_for('home'))
         else:
-            flash("incorrect username and password combination")
-    return redirect(url_for("login"))
+            return render_template('login.html', msg = 'incorrect username and password combination')
+
+def hashp(password):
+    return hashlib.sha512(password).hexdigest()
 
 #dev only
-@app.route('/seat')
+@app.route('/seat/')
 def seat():
     return render_template('seat.html')
 
