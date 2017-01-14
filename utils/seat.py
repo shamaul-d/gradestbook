0cdf8d16009d760db79a8068fc208505f15392d9
VannaM <vmavromatis18@gmail.com>
2017-01-13 23:42:37 -0500

    edit v view, attendance

--- utils/seat.py
+++ utils/seat.py
@@ -1,18 +1,33 @@
-import sqlite3   #enable control of an sqlite database
-
-#gets list of students in class classId
-def students(classId):
-    f = "utils/data/database.db"
-    db = sqlite3.connect(f)
-    c = db.cursor()
-    q = "SELECT name"
-
 def seatHtml(x, y):
     html = "";
     for i in range(y):
-        html += "<center>"
+        html += '<center>'
         for j in range(x):
+
             me = (x*i) + j
-            html += '<div class="seat" ondrop="drop(event)"><p draggable="true" ondragstart="dragStart(event)" ondragover="allowDrop(event)">Student ' + str(me) + '</p></div>'
+
+            #open div; seat
+            html += '<div class="seat" ondrop="drop(event)">'
+
+            #p; student
+            html +='<p class="student" ondragstart="dragStart(event)" ondragover="allowDrop(event)">'
+            html += 'Student ' + str(me)
+            #html += '<p class="student"> Student ' + str(me) + "</p>"
+            html += '</p>'
+
+            #attend; attendance box
+            html += '<div class="attend">'
+            html += '<input class="check" type="checkbox">'
+            html += 'Absent?'
+            html += '</input>'
+            html += '</div>'
+            
+            #close div, add aesthetic spaces
+            html += '</div>'
+            html += '&emsp;&emsp;'
+
         html += "</center><br>"
     return html;
+
+
+'<font class="attend"><input type="checkbox">Absent?</input></font>'
