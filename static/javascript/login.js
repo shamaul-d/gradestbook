909a8e432988afa1be0687a9aa8fde493437a4e1
IAsWell <jyang1737@gmail.com>
2017-01-18 14:54:57 -0500

    f

--- static/javascript/login.js
+++ static/javascript/login.js
@@ -1,18 +1,18 @@
 var login = document.getElementById('loginL');
 var reg = document.getElementById('loginR');
-
+1
 var displayLogin = function(event){
     document.getElementById('loginElements').style.display = 'block';
     document.getElementById('registerElements').style.display = 'none';
-    //login.style.backgroundColor = '#9F81F7';
-    //reg.style.backgroundColor = '#D1D5DE';
+    login.classList.add('active');
+    reg.classList.remove('active');
 }
 
 var displayReg = function(event){
     document.getElementById('loginElements').style.display = 'none';
     document.getElementById('registerElements').style.display = 'block';
-   // reg.style.backgroundColor = '#9F81F7';
-    //login.style.backgroundColor = '#D1D5DE';
+    reg.classList.add('active');
+    login.classList.remove('active');
 }
 
 login.addEventListener('click',displayLogin);
