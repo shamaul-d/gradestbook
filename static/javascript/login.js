e06ba1e3611367c6e806761131b670835ca5b6e4
IAsWell <jyang1737@gmail.com>
2017-01-18 14:10:49 -0500

    f

--- static/javascript/login.js
+++ static/javascript/login.js
@@ -4,15 +4,15 @@ var reg = document.getElementById('loginR');
 var displayLogin = function(event){
     document.getElementById('loginElements').style.display = 'block';
     document.getElementById('registerElements').style.display = 'none';
-    login.style.backgroundColor = '#9F81F7';
-    reg.style.backgroundColor = '#D1D5DE';
+    //login.style.backgroundColor = '#9F81F7';
+    //reg.style.backgroundColor = '#D1D5DE';
 }
 
 var displayReg = function(event){
     document.getElementById('loginElements').style.display = 'none';
     document.getElementById('registerElements').style.display = 'block';
-    reg.style.backgroundColor = '#9F81F7';
-    login.style.backgroundColor = '#D1D5DE';
+   // reg.style.backgroundColor = '#9F81F7';
+    //login.style.backgroundColor = '#D1D5DE';
 }
 
 login.addEventListener('click',displayLogin);
