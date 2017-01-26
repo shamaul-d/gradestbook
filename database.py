0713073760f309ff5fc841eefd85200adc8be6a4
VannaM <vmavromatis18@gmail.com>
2017-01-26 00:58:29 -0500

    COMMIT

--- database.py
+++ database.py
@@ -266,8 +266,8 @@ def absencecheck(classid,studentid,date):
 def addabsence(classid,studentid,date):
     f = "utils/data/database.db"
     db = sqlite3.connect(f)
-    if(not classcheck(classid,studentid)): # if student is in class
-        if(absencecheck(classid,studentid,date)):
+    if(not classcheck(int(classid),int(studentid))): # if student is in class
+        if(absencecheck(int(classid),int(studentid),str(date))):
             c = db.cursor()
             q = "INSERT INTO absences VALUES ('"+str(classid)+"','"+str(studentid)+"','"+str(date)+"');"
             c.execute(q)
@@ -325,7 +325,7 @@ def addperiod(classid,teacherid,period,rows,cols,classname):
     addpd(classid,teacherid,period,rows,cols,classname,code)
 
 #addperiod(9,-3,9,5,7,"chorus")
-    
+
 # return class id; 0 if nonexistent
 def classauth(code):
     if(codecheck(code)):
@@ -618,7 +618,7 @@ def getperiod(classid):
         return a[0]
 
 #print getperiod(12)
-    
+
 ##################################################################################################
 
 # changes seat of student
